39a25ad5d95cb4f2b4d72b725c4a06039bcb45bf
Yeabsera <yeabsera0830@gmail.com>
2020-05-20 11:32:31 +0300

    Added view all order in logistics

--- src/pages/Logistics/ViewAllOrdersPage.js
+++ src/pages/Logistics/ViewAllOrdersPage.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import Page from '../../components/Page';
+import { MdAssignment } from "react-icons/md";
 import { Card, CardBody, CardHeader, Button, Table } from 'reactstrap';
+import PageSpinner from '../../components/PageSpinner'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import actions from '../../store/sales/action'
+import routes from '../../config/routes'
+
+const Order = ({ order, index }) => {
+    return (
+        <tbody>
+            <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{order.customer}</td>
+                <td>{order.salesPerson}</td>
+                <td>{order.shipmentAddress}</td>
+                <td>{order.status}</td>
+                <td align='center'>
+                    <Button size='sm' color='primary'>
+                        <MdAssignment /> Generate
+                    </Button>
+                </td>
+                <td>
+                    <Link to={{ pathname: routes.ViewSingleOrderPage, state: order }}>
+                        <Button size='sm' color='primary'>
+                            See Order
+                        </Button>
+                    </Link>
+                </td>
+            </tr>
+        </tbody>
+    )
+}
 
 class ViewAllOrdersPage extends Component {
     constructor(props) {
         super(props);
         this.state = {}
     }
+
+    componentDidMount() {
+        this.props.getAllOrder()
+    }
+
     render() {
+        if (this.props.loading) return <PageSpinner />
+        if (this.props.orders.length === 0) return <h2>No orders to show</h2>
         return (
             <Page
-                title="Orders"
+                title="All Orders"
                 breadcrumbs={[{ name: 'All Orders', active: true }]}
                 className="TablePage">
                 <Card className="mb-3">
@@ -24,31 +63,15 @@ class ViewAllOrdersPage extends Component {
                                     <th>Sales Person</th>
                                     <th>Shipment Address</th>
                                     <th>Status</th>
-                                    <th>Generate Invoice</th>
+                                    <th style={{ margin: "auto" }}>Generate Invoice</th>
                                     <th>Actions</th>
-
                                 </tr>
                             </thead>
-                            <tbody>
-                                <tr align='left'>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>Mark</td>
-                                    <td>
-                                        <Button size='sm' color='primary'>
-                                            {/* <MdAssignment /> */}
-                                            Hello
-                                        </Button>
-                                    </td>
-                                    <td>
-                                        <Button size='sm' color='primary'>
-                                            See Order
-                                            </Button>
-                                    </td>
-                                </tr>
-                            </tbody>
+                            
+                                {this.props.orders.map((item, index) => (
+                                    <Order order={item} key={index} index={index} />
+                                ))}
+                            
                         </Table>
                     </CardBody>
                 </Card>
@@ -57,4 +80,22 @@ class ViewAllOrdersPage extends Component {
     }
 }
 
-export default ViewAllOrdersPage;
+
+const mapStateToProps = (state) => {
+    return {
+        loading: state.salesReducer.loading,
+        errors: state.salesReducer.errors,
+        items: state.salesReducer.items,
+        companys: state.salesReducer.companys,
+        success: state.salesReducer.success,
+        orders: state.salesReducer.orders
+    }
+}
+const mapDispatchToProps = {
+    createOrder: actions.createOrder,
+    getAllItem: actions.getAllItem,
+    getAllCompany: actions.getAllCompany,
+    getAllOrder: actions.getAllOrder
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ViewAllOrdersPage)
